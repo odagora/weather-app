8f523f17a2c0665cf7fc1a37906b3a8a6064bf31
Daniel <oscar.d.gonzalez@gmail.com>
2022-03-24 13:12:41 -0500

    feat: build list item component dynamically

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".ajax-section .cities");
 
 const apiKey = "95c34dd0a87849a370c411d72886b952";
 
@@ -17,9 +18,33 @@ form.addEventListener("submit", e => {
             const response = await fetch(url);
             const data = await response.json();
             console.log(data);
+
+            const { main, name, sys,  weather } = data;
+            const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+
+            const li = document.createElement("li");
+            li.classList.add("city");
+
+            const markup = `
+                <h2 class="city-name" data-name="${name},${sys.country}">
+                    <span>${name}</span>
+                    <sup>${sys.country}</sup>
+                </h2>
+                <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+                <figure>
+                    <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
+                    <figcaption>${weather[0]["description"]}</figcaption>
+                </figure>
+            `;
+
+            li.innerHTML = markup;
+            list.appendChild(li);
         } catch {
             msg.textContent = "Please search for a valid city 😩";
         }
     }());
+    msg.textContent = "";
+    form.reset();
+    input.focus();
 });
 
