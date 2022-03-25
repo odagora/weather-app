516caaea5ae036b7816548538a63720532a55cfb
Daniel <oscar.d.gonzalez@gmail.com>
2022-03-25 12:15:31 -0500

    feat: prevent duplicate requests

--- index.js
+++ index.js
@@ -7,7 +7,39 @@ const apiKey = "95c34dd0a87849a370c411d72886b952";
 
 form.addEventListener("submit", e => {
     e.preventDefault();
-    const inputVal = input.value;
+    let inputVal = input.value;
+
+    //Check if there's already a city
+    const listItems = document.querySelectorAll(".ajax-section .city");
+    const listItemsArray = Array.from(listItems);
+
+    if (listItemsArray.length > 0) {
+        const filteredArray = listItemsArray.filter(el => {
+            let content = "";
+            //Bogota,CO
+            if (inputVal.includes(",")) {
+                //Bogota,COOOO -> invalid country code, so we keep only the first part of inputVal
+                if (inputVal.split(",")[1].length > 2) {
+                    inputVal = inputVal.split(",")[0];
+                    content = el.querySelector(".city-name span").textContent.toLowerCase();
+                } else {
+                    content = el.querySelector(".city-name").dataset.name.toLowerCase();
+                }
+            } else {
+                //bogota
+                content = el.querySelector(".city-name span").textContent.toLowerCase();
+            }
+            return content == inputVal.toLowerCase();
+        });
+        console.log(filteredArray);
+
+        if (filteredArray.length > 0) {
+            msg.textContent = `You already know the weather for ${filteredArray[0].querySelector(".city-name span").textContent}... otherwise be more specific by providing the country code as well 😉`;
+            form. reset();
+            input.focus();
+            return;
+        }
+    }
 
     //Ajax Request
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
@@ -17,10 +49,9 @@ form.addEventListener("submit", e => {
         try {
             const response = await fetch(url);
             const data = await response.json();
-            console.log(data);
 
             const { main, name, sys,  weather } = data;
-            const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+            const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`;
 
             const li = document.createElement("li");
             li.classList.add("city");
